505b2abc5731a37dc907c87d8bab69731418c4fb
Martin Lenart <martin_lenart@icloud.com>
2022-01-11 17:06:54 +0100

    Add project files.

---
+++ BehaviorsDemo/BehaviorsDemo/Behaviors/NumericValidationBehavior.cs
@@ -0,0 +1,26 @@
+﻿using Xamarin.Forms;
+
+namespace BehaviorsDemo.Behaviors
+{
+	public class NumericValidationBehavior : Behavior<Entry>
+	{
+		protected override void OnAttachedTo (Entry entry)
+		{
+			entry.TextChanged += OnEntryTextChanged;
+			base.OnAttachedTo (entry);
+		}
+		protected override void OnDetachingFrom (Entry entry)
+		{
+			entry.TextChanged -= OnEntryTextChanged;
+			base.OnDetachingFrom (entry);
+		}
+
+		void OnEntryTextChanged (object sender, TextChangedEventArgs args)
+		{
+			double result;
+			bool isValid = double.TryParse (args.NewTextValue, out result);
+			((Entry)sender).TextColor = isValid ? Color.Green : Color.Red;
+		}
+	}
+}
+
---
+++ BehaviorsDemo/BehaviorsDemo/Views/NumericValidationPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms;
+
+namespace BehaviorsDemo.Views
+{
+	public partial class NumericValidationPage : ContentPage
+	{
+		public NumericValidationPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
